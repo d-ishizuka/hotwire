95a7c326c2c228d83be6421a229b8b101f497db4
d-ishizuka <vbstkaksa.0716@gmail.com>
2023-05-22 12:15:12 +0900

    Update test code

--- quote-editor/test/system/quotes_test.rb
+++ quote-editor/test/system/quotes_test.rb
@@ -1,6 +1,10 @@
 require "application_system_test_case"
 
 class QuotesTest < ApplicationSystemTestCase
+  setup do
+    @quote = quotes(:first)
+  end
+  
   test "Creating a new quote" do
     # When we visit the Quotes#index page
     # we expect to see a title with the text "Quotes"
@@ -22,4 +26,33 @@ class QuotesTest < ApplicationSystemTestCase
   # and to see our "Capybara Quote" in the list of quotes
   assert_selector "h1", text: "Quotes"
   assert_text "Capybara Quote"
+
+  test "Showing a quote" do
+    visit quotes_path
+    click_link @quote.name
+
+    assert_selector "h1", text: @quote.name
+  end
+
+  test "Updating a quote" do
+    visit quotes_path
+    assert_selector "h1", text: "Quotes"
+
+    click_on "Edit", match: :first
+    assert_selector "h1", text: "Edit Quote"
+
+    fill_in "Name", with: "Updated quote"
+    click_on "Update Quote"
+
+    assert_selector "h1", text: "Quotes"
+    assert_text "Updated quote"
+  end
+
+  test "Destroying a quote" do
+    visit quotes_path
+    assert_text @quote.name
+
+    click_on "Delete", match: :first
+    assert_no_text @quote.name
+  end
 end
