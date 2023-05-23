841ffe32c07581505de20552088867cabb64b9ff
d-ishizuka <vbstkaksa.0716@gmail.com>
2023-05-23 21:48:17 +0900

    Fix test code

--- quote-editor/test/system/quotes_test.rb
+++ quote-editor/test/system/quotes_test.rb
@@ -5,27 +5,27 @@ class QuotesTest < ApplicationSystemTestCase
     @quote = quotes(:first)
   end
   
-  test "Creating a new quote" do
+  test "Creating a new quote" do 
     # When we visit the Quotes#index page
     # we expect to see a title with the text "Quotes"
-    visit quotes_url
+    visit quotes_path
     assert_selector "h1", text: "Quotes"
+
+    # When we click on the link with the tet "New Quote"
+    # we expect to land on a page with the title "New Quote"
+    click_on "New quote"
+    assert_selector "h1", text: "New quote"
+
+    # When we fill in the name input with "Capybara Quote"
+    # and we click on "Create Quote"
+    fill_in "Name", with: "Capybara Quote"
+    click_on "Create Quote"
+
+    # We expect to be back on the page with the title "Quotes"
+    # and to see our "Capybara Quote" in the list of quotes
+    assert_selector "h1", text: "Quotes"
+    assert_text "Capybara Quote"
   end
-
-  # When we click on the link with the tet "New Quote"
-  # we expect to land on a page with the title "New Quote"
-  click_on "New Quote"
-  assert_selector "h1", text: "New Quote"
-
-  # When we fill in the name input with "Capybara Quote"
-  # and we click on "Create Quote"
-  fill_in "Name", with: "Capybara Quote"
-  click_on "Create Quote"
-
-  # We expect to be back on the page with the title "Quotes"
-  # and to see our "Capybara Quote" in the list of quotes
-  assert_selector "h1", text: "Quotes"
-  assert_text "Capybara Quote"
 
   test "Showing a quote" do
     visit quotes_path
@@ -39,7 +39,7 @@ class QuotesTest < ApplicationSystemTestCase
     assert_selector "h1", text: "Quotes"
 
     click_on "Edit", match: :first
-    assert_selector "h1", text: "Edit Quote"
+    assert_selector "h1", text: "Edit quote"
 
     fill_in "Name", with: "Updated quote"
     click_on "Update Quote"
